863efaf855c3ade5548c4fb973b8f0fc2e79431b
Dvonne Daniels <81335875+dvonnedaniels@users.noreply.github.com>
2025-06-26 16:12:25 -0400

    Add files via upload

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,37 @@
+// This is a function.
+// console is the object.
+// log is the method.
+// 100 is an argument.
+// 100 is a type of number.
+
+// Shortcuts
+// ctrl + // will setup a comment
+// ctrl + // will toggle a comment as well
+
+
+
+console.log(100);
+
+console.log('Hello, World!')
+
+console.log(20, 'Hello', true);
+
+const x = 100;
+
+console.log(x);
+
+console.error('Alert');
+
+console.warn('Warning!');
+
+console.table({ name: 'Dvonne', email: 'dvonne.daniels@udri.udayton.edu'})
+
+console.group('simple')
+console.log(x);
+console.error('Alert');
+console.warn('Warning!');
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color: white; color: green';
+
+console.log('%cHello World', styles);
