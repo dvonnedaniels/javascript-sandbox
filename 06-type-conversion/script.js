4f009f0f073f0f196d2bf17cdb0d26db0624b7f6
Dvonne Daniels <dvonne.daniels@udri.udayton.edu>
2025-06-30 13:33:31 -0400

    Added type conversions

---
+++ 06-type-conversion/script.js
@@ -0,0 +1,22 @@
+let amount = 'hello';
+
+// Change string to number
+amount = parseInt(amount);
+// amount = +amount;
+// amount = Number(amount);
+
+// Change number to string
+// amount = amount.toString();
+// amount = String(amount);
+
+// Convert string to decimal
+// amount = parseFloat(amount);
+
+// Convert nuumber to boolean
+// amount = Boolean(amount);
+console.log(Math.sqrt(-1));
+console.log(1 + NaN);
+console.log(undefined + undefined)
+console.log('foo' / 3);
+
+console.log(amount, typeof amount);
